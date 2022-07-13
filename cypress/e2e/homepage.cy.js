979a4ab7b3322620ab38d245e6373df5bf774476
Salamon <salamongetahon@gmail.com>
2022-07-13 19:15:48 +0300

    Add deploy script gitactions 4

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -2,5 +2,5 @@ describe('The Home Page', () => {
   it('visits the home page', () => {
     cy.visit('http://35.205.56.34//')
   })
-  // This is a comment for the new branch 
+  // This is a comment for the new branch
 })

c04b549e0cbed7dbe3dec0a4a596b39f3ee6241b
salamon1470 <86660010+salamon1470@users.noreply.github.com>
2022-07-13 13:33:17 +0300

    Revert "testing a failing test for PR"

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,6 +1,6 @@
 describe('The Home Page', () => {
   it('visits the home page', () => {
-    cy.visit('http://35.205.56.35//')
+    cy.visit('http://35.205.56.34//')
   })
   // This is a comment for the new branch
 })

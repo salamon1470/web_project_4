f5127ef65596d59d9177aeb5b38cc62554e725b6
Salamon <salamongetahon@gmail.com>
2022-07-13 13:56:39 +0300

    changes to test file 2

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -1,10 +1,6 @@
 describe('The Home Page', () => {
   it('visits the home page', () => {
-<<<<<<< HEAD
-    cy.visit('http://35.205.56.34//')
-=======
     cy.visit('http://35.205.56.36//')
->>>>>>> cfecc8f (failing the cypess for PR)
   })
   // This is a comment for the new branch
 })
